b5409b8732ee4e16e365297365b3d67c93c8a61b
raaj2019 <52959140+raaj2019@users.noreply.github.com>
2019-07-16 16:27:42 +0530

    Add files via upload

---
+++ Assignment2/operation.java
@@ -0,0 +1,41 @@
+
+public class operation 
+{
+	
+	 public void add(int a,int b)
+	{
+		int sum = (a+b);
+		System.out.println("Sum of two number is"+sum);
+	}
+	
+	 public void add(int a,int b,int c)
+		{
+			int sum = (a+b+c);
+			System.out.println("Sum of three number is: "+sum);
+		}
+	 public void sub(int a,int b)
+		{
+			int sub = (a-b);
+			System.out.println("Subtraction of two number is: "+sub);
+		}
+	 public void sub(int a,int b, int c)
+		{
+			int sub = (a-b-c);
+			System.out.println("Subtraction of three number is: "+sub);
+		}
+	 public void mul(int a,int b)
+		{
+			int mul = (a*b);
+			System.out.println("Multiplication of two number is: "+mul);
+		}
+	 public void mul(int a,int b,int c)
+		{
+			int mul = (a*b*c);
+			System.out.println("Multiplication of three number is: "+mul);
+		}
+	 public void div(int a,int b)
+		{
+			int div = (a/b);
+			System.out.println("Division of two number is: "+div);
+		}
+}
